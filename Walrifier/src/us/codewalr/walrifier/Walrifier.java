60fecf8e8a1b009cfbee7a51b54e1ed0cc948eb2
RobinDeWalvis <voetterrb1a@gmail.com>
2015-05-08 17:06:59 +0200

    CardView support library fix

--- Walrifier/src/us/codewalr/walrifier/Walrifier.java
+++ Walrifier/src/us/codewalr/walrifier/Walrifier.java
@@ -1,15 +1,38 @@
 package us.codewalr.walrifier;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 public class Walrifier extends Activity
 {
+	static Walrifier instance;
+	
+	View view;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		instance = this;
+		
+		setView(new WalrifierView(this));
 	}
 	
+	public static Walrifier getInstance()
+	{
+		return instance;
+	}
 	
+	public static Context getContext()
+	{
+		return instance;
+	}
+	
+	public void setView(View v)
+	{
+		this.view = v;
+		setContentView(v);
+	}
 }
---
+++ Walrifier/src/us/codewalr/walrifier/WalrifierView.java
@@ -0,0 +1,12 @@
+package us.codewalr.walrifier;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+
+public class WalrifierView extends CardView
+{
+	public WalrifierView(Context context)
+	{
+		super(context);
+	}
+}
