bf1966d4a36ee0f978948f6d31fe84e02d699a90
RobinDeWalvis <voetterrb1a@gmail.com>
2015-05-08 17:48:56 +0200

    Cardview support remove & Recycler support fix
    Hihi, we hadden niet cardview nodig maar recyclerview :3

--- Walrifier/src/us/codewalr/walrifier/WalrifierView.java
+++ Walrifier/src/us/codewalr/walrifier/WalrifierView.java
@@ -1,12 +1,17 @@
 package us.codewalr.walrifier;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 
-public class WalrifierView extends CardView
+public class WalrifierView extends RecyclerView
 {
 	public WalrifierView(Context context)
 	{
 		super(context);
+	}
+	
+	public void addCard()
+	{
+		
 	}
 }
