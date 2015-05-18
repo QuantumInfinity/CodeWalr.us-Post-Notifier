59c912dd69b1ff64382b013f4ab7308c0c9abd87
RobinDeWalvis <voetterrb1a@gmail.com>
2015-05-18 20:30:33 +0200

    gif rond kuttingen

--- Walrifier/src/us/codewalr/walrifier/util/ImageDrawable.java
+++ Walrifier/src/us/codewalr/walrifier/util/ImageDrawable.java
@@ -1,16 +1,20 @@
 package us.codewalr.walrifier.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import us.codewalr.walrifier.Walrifier;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-
-//TODO ION GIFS
+import android.util.Log;
 
 public class ImageDrawable
 {	
+	static GifDecoder gifDecoder = null;
+	
 	private ArrayList<Frame> frames;
 	private long startTime;
 	private int duration, width = 0 , height = 0;
@@ -18,26 +22,40 @@ public class ImageDrawable
 	public ImageDrawable(InputStream stream)
 	{
 		frames = new ArrayList<Frame>();
-				
-		fromSingle(stream);
+		
+		byte[] data = toArray(stream);
+		if (fromGIF(data) != GifDecoder.STATUS_OK)
+			fromSingle(data);
 		
 		computeDimensions();
 		reset();
 	}
 	
-	public void fromSingle(InputStream stream)
+	public void fromSingle(byte[] data)
 	{
-		Bitmap b = BitmapFactory.decodeStream(stream);
+		Bitmap b = BitmapFactory.decodeByteArray(data , 0, data.length);
 		b.setDensity(Bitmap.DENSITY_NONE);
 		frames.add(new Frame(b, 0));
 		duration = 0;
 	}
 	
-	public int fromGIF(InputStream stream)
+	public int fromGIF(byte[] data)
 	{
-
+		if (gifDecoder == null)
+			gifDecoder = new GifDecoder();
 		
-		return 0;
+		int status = gifDecoder.read(data);
+		
+		if (status == GifDecoder.STATUS_OK)
+			for(int i=0; i<gifDecoder.getFrameCount(); i++)
+			{
+				gifDecoder.setCurrentFrameIndex(i);
+				frames.add(new Frame(gifDecoder.getNextFrame(), gifDecoder.getNextDelay()));
+			}
+		
+		duration = getTotalTime();
+		
+		return status;
 	}
 	
 	public void draw(Canvas canvas)
@@ -46,6 +64,26 @@ public class ImageDrawable
 		Frame f = getFrame(delta);
 		if (f != null)
 			canvas.drawBitmap(f.image, 0, 0, null);
+	}
+	
+	public byte[] toArray(InputStream stream)
+	{
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		
+		try{
+		int nRead;
+		byte[] data = new byte[1024];
+
+		while ((nRead = stream.read(data, 0, data.length)) != -1)
+			buffer.write(data, 0, nRead);
+
+		buffer.flush();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return buffer.toByteArray();
 	}
 	
 	public void reset()
@@ -76,20 +114,21 @@ public class ImageDrawable
 	}
 	
 	public Frame getFrame(int time)
-	{
+	{		
 		if (frames.size() == 0)
 			return null;
+				
 		if (duration == 0 || frames.size() == 1)
 			return frames.get(0);
 		
 		time = time % duration;
 		int total = 0;
-		
-		for (Frame f:frames)
+
+		for (int i=0; i<frames.size(); i++)
 		{
-			total += f.duration;
+			total += frames.get(i).duration;
 			if (total > time)
-				return f;
+				return frames.get(i);
 		}
 		return null;
 	}
