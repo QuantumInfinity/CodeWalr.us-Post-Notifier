eb63f6c75b8837798d81b17d2769e5620d0284bd
RobinDeWalvis <voetterrb1a@gmail.com>
2015-05-28 20:20:44 +0200

    credits start

---
+++ Walrifier/src/us/codewalr/walrifier/ui/CreditsFragment.java
@@ -0,0 +1,23 @@
+package us.codewalr.walrifier.ui;
+
+import us.codewalr.walrifier.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreditsFragment extends Fragment
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.credits, container, false);
+	}
+}
